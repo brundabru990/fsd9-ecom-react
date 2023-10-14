cbe2bc682a05966f86af9f75f4d925eba537c5d0
brundabru990 <147944740+brundabru990@users.noreply.github.com>
2023-10-15 01:52:08 +0530

    Update_Login.jsx

--- src/app/pages/Authentication/Login/Login.jsx
+++ src/app/pages/Authentication/Login/Login.jsx
@@ -16,8 +16,7 @@ import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Alert, Snackbar } from "@mui/material";
-import { postApiForLogin } from "../../../Api/HttpApi";
+import { Alert } from "@mui/material";
 
 function Copyright(props) {
   return (
@@ -32,154 +31,129 @@ function Copyright(props) {
   );
 }
 
-const defaultTheme = createTheme();
+const defaultTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#007bff", 
+    },
+    secondary: {
+      main: "#ff6f61", 
+    },
+  },
+});
 
 function Login() {
-  const [open, setOpen] = useState(false);
-  const [severity,setSeverity] = useState('success');
-  const [authMsg,setAuthMsg] = useState('');
+  const { state } = useNavigation();
+  const navigate = useNavigate();
+  const actionData = useActionData();
 
-  // useEffect(() => {
-  //   if (actionData?.status === "success") {
-
-  //     localStorage.setItem("userToken", actionData.data.token);
-  //   }
-  // }, [actionData?.status, actionData?.data, navigate]);
-
-  const handleClose = (event, reason) => { 
-    setOpen(false);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const _formData = new FormData(event.currentTarget);    
-
-    postApiForLogin(
-        JSON.stringify({
-             username: _formData.get('username'), 
-            password: _formData.get('password'),
-          })
-    )
-    .then(response => {
-      setAuthMsg('Login Successfully!!');
-      setOpen(true);
-      setSeverity('success');
-       console.log(response)
-  })
-  .catch(error => {
-      let msg = error.message;
-      if(error?.response?.data?.message){
-        msg = error.response.data.message;
-      }
-      console.log(msg);
-      setAuthMsg(msg)
-      setOpen(true);    
-      setSeverity('error') 
-  });
- 
-  };
+  useEffect(() => {
+    if (actionData?.status === "success") {
+      localStorage.setItem("userToken", actionData.data.token);
+    }
+  }, [actionData?.status, actionData?.data, navigate]);
 
   return (
-    <>
-      <ThemeProvider theme={defaultTheme}>
-        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}  anchorOrigin={{'vertical':'top', 'horizontal':'center' }}>
-          <Alert severity={severity} sx={{ width: '100%' }}>
-            {authMsg}
-          </Alert>
-        </Snackbar>
-
-        <Grid container component="main" className="d-flex align-items-center justify-content-center" sx={{ height: '100vh' }}>
-          <CssBaseline />
-           
-          <Grid item xs={12} sm={8} md={4} component={Paper} elevation={6} square>
-            
-              <Box
-                sx={{
-                  my: 8,
-                  mx: 4,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                }}
-              >
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                  <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                  Sign in
-                </Typography>
-                {/* {
-                  actionData?.status === "error" && <Alert severity="error" sx={{ width: '100%' }}>
-                    {actionData?.data}
-                  </Alert>
-                } */}
-               
-                <Box component="form"  noValidate  onSubmit={handleSubmit} sx={{ mt: 1 }} >
-                  <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="username"
-                    autoComplete="email"
-                    autoFocus
-                  />
-                  <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox value="remember" color="primary" />}
-                    label="Remember me"
-                  />
-                  <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                  >
-                    Sign In
-                  </Button>
-                  <Grid container>
-                    <Grid item xs>
-                      <Link href="/forgot-password" variant="body2">
-                        Forgot password?
-                      </Link>
-                    </Grid>
-                    <Grid item>
-                      <Link href="/register" variant="body2">
-                        {"Don't have an account? Sign Up"}
-                      </Link>
-                    </Grid>
+    <ThemeProvider theme={defaultTheme}>
+      <Grid
+        container
+        component="main"
+        sx={{
+          height: '100vh',
+          background: 'linear-gradient(to bottom, #007bff, #50a5e1)',
+          alignItems: 'center',
+          justifyContent: 'center',
+          display: 'flex',
+        }}
+      >
+        <CssBaseline />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Form method="POST">
+            <Box
+              sx={{
+                my: 8,
+                mx: 4,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h4" style={{ color: 'primary.main' }}>
+                Welcome Back!
+              </Typography>
+              {actionData?.status === "error" && (
+                <Alert severity="error" sx={{ width: '100%' }}>
+                  {actionData?.data}
+                </Alert>
+              )}
+              <Box component="form" sx={{ mt: 1 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="username"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2, backgroundColor: 'primary.main', color: 'white' }}
+                >
+                  Sign In
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="/forgot-password" variant="body2">
+                      Forgot password?
+                    </Link>
                   </Grid>
-                  <Copyright sx={{ mt: 5 }} />
-                </Box>
+                  <Grid item>
+                    <Link href="/register" variant="body2">
+                      {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
+                </Grid>
+                <Copyright sx={{ mt: 5 }} />
               </Box>
-          
-          </Grid>
+            </Box>
+          </Form>
         </Grid>
-      </ThemeProvider>
-    </>
+      </Grid>
+    </ThemeProvider>
   );
 }
 
-// export async function action({ request }) {
-//   const formData = await request.formData();
-//   const data = Object.fromEntries(formData);
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
 
-//   try {
-//     const res = await axios.post("http://localhost:8080/api/v1/auth/login", data);
-//     return { status: "success", data: res.data };
-//   } catch (err) {
-//     return { status: "error", data: err?.response?.data?.message };
-//   }
-// }
+  try {
+    const res = await axios.post("http://localhost:8080/api/v1/auth/login", data);
+    return { status: "success", data: res.data };
+  } catch (err) {
+    return { status: "error", data: err?.response?.data?.message };
+  }
+}
 
 export default Login;
