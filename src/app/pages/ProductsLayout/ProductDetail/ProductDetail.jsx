2973b4832964cc2d91d704d1c86074fc974647ac
mvasim <mvasim>
2023-10-15 00:54:01 +0530

    update

--- src/app/pages/ProductsLayout/ProductDetail/ProductDetail.jsx
+++ src/app/pages/ProductsLayout/ProductDetail/ProductDetail.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router";
 import { fetchProductDetail } from "../../../Api/HttpApi";
 import './ProductDetail.css';
 import { Breadcrumbs, Rating, Typography } from "@mui/material";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 
 const ProductDetail = () => {
 
@@ -27,72 +27,89 @@ const ProductDetail = () => {
 
     return (
         <>
-        {dataLoaded ? (
-            <div className="container col-xxl-8 px-4 py-5">
-            <div className="row flex-lg-row align-items-center g-5 py-5">
-                <div className="col-10 col-sm-8 col-lg-6">
-                    <img src={product.imgUrl} className="product_imgUrl d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" loading="lazy" />
-                    <div className="d-grid gap-2 d-md-flex py-4 justify-content-md-center">
-                        <button type="button" className="btn  btn-outline-secondary  btn-lg px-4 me-md-2">Add To wishlisth</button>
-                        <button type="button" className="btn btn-primary btn-lg px-4">Add To Cart</button>
+            {dataLoaded ? (
+                <div className="container col-xxl-8   py-5">
+                    <div className="row flex-lg-row align-items-center-d g-5 py-5">
+                        <div className="col-10 col-sm-12 col-lg-6">
+                            <div className="row flex-lg-row ">
+                                <div className="col-3 col-lg-3 col-sm-6"> 
+                                <div className="card">
+                                    <ul className="list-group list-group-flush">
+                                    {product.eoProductImgArray.map(_img => (
+                                        <li className="list-group-item"> 
+                                        <img src={_img.imgUrl} className="product-image" alt="ProductImage"></img>
+                                        </li> 
+                                    ))}
+                                       
+                                    </ul> 
+                                    </div>
+                                </div>
+                                <div className="col-9 col-lg-9 col-sm-6">
+                                <img src={product.imgUrl} className="product_imgUrl d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" loading="lazy" />
+                                    <div className="d-grid gap-2 d-md-flex py-4 justify-content-md-center">
+                                        <button type="button" className="btn  btn-outline-secondary  btn-lg px-4 me-md-2">Add To wishlisth</button>
+                                        <button type="button" className="btn btn-primary btn-lg px-4">Add To Cart</button>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div className="col-lg-6">
+                            <Breadcrumbs aria-label="breadcrumb">
+                                <Link
+                                    underline="hover"
+                                    color="inherit"
+                                    to="/"
+                                >
+                                    Home
+                                </Link>
+                                <Link underline="hover" color="inherit" to={`../../category/${product.eoProductCategory.id}`}
+                                >
+                                    {product.eoProductCategory?.name}
+                                </Link>
+                                {/* <Typography color="text.primary text-truncate">{product?.name}</Typography> */}
+                            </Breadcrumbs>
+                            <div className="fw-bold fs-4 lh-2 mb-3">{product.name}</div>
+                            <div>
+
+                                <Rating name="read-only" value={product?.rating} readOnly />
+                            </div>
+
+                            <h1 className="card-title pricing-card-title">₹ {product.price}</h1>
+                            <div className="container d-flex">
+                                <div className="col-md-3 fw-bold  d-flex  p-3">
+                                    Highlights
+                                </div>
+                                <div className="col-md-9 p-3" style={{ lineHeight: 2 }} dangerouslySetInnerHTML={{ __html: product.keyFeatures }}>
+
+                                </div>
+                            </div>
+
+                            <div className="container d-flex">
+                                <div className="col-md-3 d-flex fw-bold  p-3">
+                                    Seller
+                                </div>
+                                <div className="col-md-9 p-3 fw-bold text-primary" style={{ lineHeight: 2, fontStyle: 'italic' }}>
+                                    {product.sellerUser?.firstName}
+                                </div>
+                            </div>
+                            <div className="container d-flex">
+                                <div className="col-md-3 fw-bold d-flex p-3 ">
+                                    Description
+                                </div>
+                                <div className="col-md-9 p-3" style={{ lineHeight: 2, fontStyle: 'italic' }}>
+                                    {product.description}
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
-                <div className="col-lg-6">
-                    <Breadcrumbs aria-label="breadcrumb">                            
-                        <Link
-                            underline="hover"
-                            color="inherit"
-                            to="/"
-                        >
-                            Home
-                        </Link>
-                        <Link underline="hover" color="inherit"  to={`../../category/${product.eoProductCategory.id}`}
-                          >
-                            {product.eoProductCategory?.name}
-                        </Link>
-                        <Typography color="text.primary text-truncate">{product?.name}</Typography>
-                    </Breadcrumbs>
-                    <div className="fw-bold fs-4 lh-2 mb-3">{product.name}</div>
-                    <div>
-                        
-                        <Rating name="read-only" value={product?.rating} readOnly />
-                    </div>
+            ) : (
+                <div>Loading...</div>
+            )}
 
-                    <h1 className="card-title pricing-card-title">₹ {product.price}</h1>
-                    <div className="container d-flex">
-                        <div className="col-md-3 fw-bold  d-flex  p-3">
-                            Highlights
-                        </div>
-                        <div className="col-md-9 p-3" style={{ lineHeight: 2 }} dangerouslySetInnerHTML={{ __html: product.keyFeatures }}>
 
-                        </div>
-                    </div>
-
-                    <div className="container d-flex">
-                        <div className="col-md-3 d-flex fw-bold  p-3">
-                            Seller
-                        </div>
-                        <div className="col-md-9 p-3 fw-bold text-primary" style={{ lineHeight: 2, fontStyle: 'italic' }}>
-                            {product.sellerUser?.firstName}
-                        </div>
-                    </div>
-                    <div className="container d-flex">
-                        <div className="col-md-3 fw-bold d-flex p-3 ">
-                            Description
-                        </div>
-                        <div className="col-md-9 p-3" style={{ lineHeight: 2, fontStyle: 'italic' }}>
-                            {product.description}
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-        ): (
-           <div>Loading...</div>
-       ) }
-
-            
 
         </>
     )
